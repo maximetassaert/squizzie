ea62a2fdac216b52f0ea4263877b65cd50f1ecb4
francoismk <francois.szymczak@adeo.com>
2023-09-22 14:35:12 +0200

    feat: login

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,14 @@
-import Image from 'next/image'
+"use client"
 import { Button } from "@/components/ui/button"
+import { Nav } from '@/components/main-nav'
+import Link from 'next/link';
+import * as React from "react"
 
 export default function Home() {
   return (
-    <Button>Coucou petite perruche</Button>
+    <header className='p-4 flex justify-between border-solid border-2 border-slate-200/10 rounded-lg'>
+    <h1 className='font-bold text-3xl text-slate-800'><Link href="./">Squizzie</Link></h1>
+      <Nav/>
+      </header>
   )
 }
